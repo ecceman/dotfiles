fabaf83ec166c98c8f874e568ac1c03c2044d5b8
ecceman <39990046+ecceman@users.noreply.github.com>
2019-12-26 08:09:14 +0100

    Update wlan-info.py

--- polybar/wlan-info.py
+++ polybar/wlan-info.py
@@ -4,17 +4,19 @@ import subprocess
 import re
 from pathlib import Path
 
+wnic = "wlp61s0"
+
 cmd_ap = subprocess.Popen(['nmcli -f BSSID,ACTIVE dev wifi list | awk \'$2 ~ /yes/ {print $1}\''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 ap = cmd_ap.stdout.read().decode('utf-8').strip('\n').lower()
 
-cmd_rssi = subprocess.Popen(['cat /proc/net/wireless | grep wlp4s0 | cut -d " " -f 7 | head -c 3'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+cmd_rssi = subprocess.Popen(['cat /proc/net/wireless | grep ' + wnic + ' | cut -d " " -f 7 | head -c 3'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 rssi = cmd_rssi.stdout.read().decode('utf-8')
 
-cmd_ch = subprocess.Popen(['sudo iwlist wlp4s0 frequency |  grep Current | awk \'{print substr($5,0,2)}\''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+cmd_ch = subprocess.Popen(['sudo iwlist ' + wnic + ' frequency |  grep Current | awk \'{print substr($5,0,2)}\''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 ch = cmd_ch.stdout.read().decode('utf-8')
 ch = re.sub("[^0-9]", "", ch)
 
-cmd_ht = subprocess.Popen(['sudo iw dev | grep -i wlp4s0 -A 10 | grep -w "width: [0-9]*" | awk \'{print $6}\''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+cmd_ht = subprocess.Popen(['sudo iw dev | grep -i ' + wnic + ' -A 10 | grep -w "width: [0-9]*" | awk \'{print $6}\''], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 ht = cmd_ht.stdout.read().decode('utf-8')
 ht = ht.strip('\n')
 
