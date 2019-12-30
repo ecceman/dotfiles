341719e1537a74b61d9829e8c36f8d141d7ed1e7
ecceman <39990046+ecceman@users.noreply.github.com>
2019-12-30 14:38:30 +0100

    polybar multipe monitors launcher

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Terminate already running bar instances
+# Kill current instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar -r example &
-
-echo "Polybar launched..."
+# Launch polybar for all connected monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    polybar --reload $m &
+  done
+else
+  polybar --reload eDP1 &
+fi
